0f38c459e0f5cbbe6862aa37189e8e48c430cfbe
HiroIshida <spitfire.docomo@gmail.com>
2020-03-22 17:20:40 +0900

    change name Frame -> Link

--- basic.jl
+++ basic.jl
@@ -8,39 +8,39 @@ mutable struct Revolute
   end
 end
 
-mutable struct Frame
+mutable struct Link
   parent
   child
   name
-  function Frame(name)
+  function Link(name)
     new(Nothing, Nothing, name)
   end
 end
 
 
 mutable struct Robot
-  frames 
+  links 
   joints
   tfs 
   q 
-  function Robot(frame_list, joint_list)
+  function Robot(link_list, joint_list)
     joints = Dict()
 
-    frames = Dict()
-    for fr in frame_list
-      frames[fr.name] = fr
+    links = Dict()
+    for fr in link_list
+      links[fr.name] = fr
     end
 
-    println(frames)
+    println(links)
     for jt in joint_list
       joints[jt.name] = jt
-      frames[jt.parent].child = jt
-      frames[jt.child].parent = jt
+      links[jt.parent].child = jt
+      links[jt.child].parent = jt
     end
 
     tfs = Nothing
     q = Nothing
-    new(frames, joints, tfs, q)
+    new(links, joints, tfs, q)
   end
 end
 
@@ -66,34 +66,34 @@ function set_configuration(r::Robot, q)
   r.q = q
 end
 
-function get_parent_frame(r::Robot, frame::Frame)
-  joint_parent = r.joints[frame.parent.name]
-  frame_parent = r.frames[joint_parent.parent]
+function get_parent_link(r::Robot, link::Link)
+  joint_parent = r.joints[link.parent.name]
+  link_parent = r.links[joint_parent.parent]
 end
 
-function get_tf(r::Robot, frame_desc_name, frame_asc_name)
-  key_try = (frame_desc_name, frame_asc_name)
+function get_tf(r::Robot, link_desc_name, link_asc_name)
+  key_try = (link_desc_name, link_asc_name)
   try
     tf = r.tfs[key_try]
     return tf
   catch error
     if isa(error, KeyError) # key not found
-      frame_asc = r.frames[frame_asc_name]
-      frame_desc = r.frames[frame_desc_name]
+      link_asc = r.links[link_asc_name]
+      link_desc = r.links[link_desc_name]
 
-      function recursion(vec, frame)
-        frame == frame_asc && (return vec)
+      function recursion(vec, link)
+        link == link_asc && (return vec)
 
-        frame_parent = get_parent_frame(r, frame)
-        name_this = frame.name
-        name_parent = frame_parent.name
+        link_parent = get_parent_link(r, link)
+        name_this = link.name
+        name_parent = link_parent.name
 
         key = (name_this, name_parent)
         tf_single = r.tfs[key] #lambda
-        recursion(tf_single(vec), frame_parent)
+        recursion(tf_single(vec), link_parent)
       end
-      tf(vec) = recursion(vec, frame_desc)
-      r.tfs[(frame_desc_name, frame_asc_name)] = tf
+      tf(vec) = recursion(vec, link_desc)
+      r.tfs[(link_desc_name, link_asc_name)] = tf
       return tf
     end
   end
@@ -102,13 +102,12 @@ end
 function visualize(r::Robot)
   points = []
   points_tip = []
-  for fr in values(r.frames)
-    print(fr.name)
-    tf = get_tf(r, fr.name, "world")
+  for link in values(r.links)
+    tf = get_tf(r, link.name, "world")
     point = tf([0, 0])
     point_tip = tf([1, 0])
     push!(points, point)
-    if fr.name != "world"
+    if link.name != "world"
       plot!([point[1], point_tip[1]], [point[2], point_tip[2]], legend=false)
     end
   end
@@ -117,21 +116,21 @@ function visualize(r::Robot)
   plot!(arr[1, :], arr[2, :], seriestype = :scatter, legend=false)
 end
 
-fr_world = Frame("world")
-fr_body1 = Frame("body1")
-fr_body2 = Frame("body2")
-fr_body3 = Frame("body3")
-fr_body4 = Frame("body4")
+link_world = Link("world")
+link_body1 = Link("body1")
+link_body2 = Link("body2")
+link_body3 = Link("body3")
+link_body4 = Link("body4")
 
 joint1 = Revolute("world", "body1", [0, 0, 0], "joint1")
 joint2 = Revolute("body1", "body2", [1, 0, 0], "joint2")
 joint3 = Revolute("body2", "body3", [1, 0, 0], "joint3")
 joint4 = Revolute("body3", "body4", [1, 0, 0], "joint4")
 
-frame_list = [fr_world, fr_body1, fr_body2, fr_body3, fr_body4]
+link_list = [link_world, link_body1, link_body2, link_body3, link_body4]
 joint_list = [joint1, joint2, joint3, joint4]
 
-r = Robot(frame_list, joint_list)
+r = Robot(link_list, joint_list)
 
 tf = get_tf(r, "body1", "world") 
 #tf([1, 0])
