2ec1acd35acdcbb09f5bc6481973b108008dc1bf
HiroIshida <spitfire.docomo@gmail.com>
2020-04-05 01:03:03 +0900

    add test for TF

--- test_transform.jl
+++ test_transform.jl
@@ -10,4 +10,15 @@ function test_rotation()
   @test norm(rot41_direct.vec - rot41.vec) < 1e-10
 end
 
+function test_transform()
+  tf21 = Transform([1., 1., 0.], [0, 0, pi/4])
+  tf32 = Transform([0., 0., 0.], [0, 0, pi/4])
+  tf43 = Transform([1., 0., 0.], [0, 0, 0])
+  tf41 = tf43 ∘ tf32 ∘ tf21
+  tf41_direct = Transform([1., 2., 0], [0, 0, pi/2])
+  @test norm(tf41.trans - [1., 2., 0]) < 1e-10
+  @test norm(tf41.rot.vec - rpy2quaternion(0, 0, pi/2)) < 1e-10
+end
+
+test_rotation()
 test_transform()
