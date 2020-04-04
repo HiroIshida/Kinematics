a58c8c4d9ee4ffe6ec2cc2f0c1058feb84f7cfeb
HiroIshida <spitfire.docomo@gmail.com>
2020-04-04 21:21:59 +0900

    finish transform 3d

--- transform.jl
+++ transform.jl
@@ -22,22 +22,27 @@ end
 
 abstract type AbstractRotation end
 
+function (rot::AbstractRotation)(x)
+  return rot.mat * x
+end
+
+
 struct Euler <: AbstractRotation
   rpy::SVector{3, Float64}
   mat::SMatrix{3, 3, Float64}
 end
 
 function Euler(r, p, y)
-  rpy = SVector{3, Float64}(r, p, y)
+  rpy = @SVector [r, p, y]
   mat = Rz(-r) * Ry(-p) * Rx(-y)
   Euler(rpy, mat)
 end
 
 mutable struct Quaternion <: AbstractRotation
-  vec
-  mat
+  vec::SVector{4, Float64}
+  mat::Union{Nothing, SMatrix{3, 3, Float64}}
   function Quaternion(x, y, z, w)
-    vec = [x, y, z, w]
+    vec = @SVector [x, y, z, w]
     new(vec, nothing)
   end
 end
@@ -52,19 +57,32 @@ function Base.getproperty(q::Quaternion, field::Symbol)
   return getfield(q, field)
 end
 
-function (rot::Euler)(x)
-  return rot.mat * x
-end
 
 struct Transform
   trans::SVector{3, Float64}
   rot::AbstractRotation
 end
 
+function Transform(vec)
+  if length(vec) == 6
+    trans = @SVector[vec[1], vec[2], vec[3]]
+    rot = Euler(vec[4], vec[5], vec[6])
+    Transform(trans, rot)
+  elseif length(vec) == 7
+    trans = @SVector[vec[1], vec[2], vec[3]]
+    rot = Quaternion(vec[4], vec[5], vec[6], vec[7])
+    Transform(trans, rot)
+  else
+    error("vec must be dim=6 or 7")
+  end
+end
+
 function (tf::Transform)(x)
   return tf.trans + tf.rot(x)
 end
 
-tf = Transform(@SVector[0., 0., 0.], Euler(0., 0., 0.5))
-v = tf([1, 0, 0])
-q = Quaternion(0, 0, 0, 1)
+tf1 = Transform([0, 0, 0, 0, 0, 0])
+tf2 = Transform([0, 0, 0, 0, 0, 0, 1])
+tf1([0, 0, 0])
+tf2([1, 1, 1])
+
